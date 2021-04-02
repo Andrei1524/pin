3c4128e6aec0cfe3f99df2e4e7f7ea1d98c54c37
Mandrican Andrei <adyta094@gmail.com>
2021-04-02 18:38:46 +0300

    Update pin.js

--- pin.js
+++ pin.js
@@ -1,20 +1,3 @@
-let box = document.querySelector('.projects-box-with-scroll')
-let arrow = document.querySelector(".selector");
-let section = document.querySelector(".projects-box-with-scroll section");
-let boxOverlay = document.querySelector('#box-overlay')
-let bottomStop = 170
-
-if (window.innerWidth < 1200) {
-  box = document.querySelector('.projects-box-without-scroll')
-  section = document.querySelector(".project-col");
-  arrow = document.querySelector(".selector-m");
-  bottomStop = 0
-}
-
-let boxBottom = box.getBoundingClientRect().bottom - section.getBoundingClientRect().height
-let boxTop = box.getBoundingClientRect().top
-let stop = (box.getBoundingClientRect().top - 200);
-
 window.onscroll = function (e) {
   let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
